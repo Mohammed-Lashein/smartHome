6a74aa0dbc415b84b84bdb01a7fce38bf12b415f
Mohammed Lashein <ben10gravattack@gmail.com>
2023-07-28 18:40:51 +0300

    finished the testimonials and the FAQ's sections

---
+++ app.js
@@ -0,0 +1,14 @@
+// Elements selection
+const toggleBtn = document.querySelector('.question-btn');
+const questions = document.querySelectorAll('.question');
+// Functions
+
+questions.forEach((question) => {
+    const toggleBtn = question.querySelector('.question-btn');
+    toggleBtn.addEventListener('click', handleActiveClass);
+    function handleActiveClass() {
+        question.classList.toggle('active');
+    }
+})
+
+// Event listeners
