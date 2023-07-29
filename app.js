082329ee6884050b9964b092ad6d12c8ee3abffd
Mohammed Lashein <ben10gravattack@gmail.com>
2023-07-29 19:36:12 +0300

    finished the footer section and styling the fixed nav

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // Elements selection
 const toggleBtn = document.querySelector('.question-btn');
 const questions = document.querySelectorAll('.question');
+
+const spanDate = document.querySelector('.date');
+
+const nav = document.querySelector('.nav');
+console.log(nav);
 // Functions
 
+// FAQs 
 questions.forEach((question) => {
     const toggleBtn = question.querySelector('.question-btn');
     toggleBtn.addEventListener('click', handleActiveClass);
@@ -10,4 +16,20 @@ questions.forEach((question) => {
         question.classList.toggle('active');
     }
 })
+
+// date of copyright
+spanDate.textContent = new Date().getFullYear();
+
+// fixing the navbar
+function handleNav() {
+    if (window.scrollY >= nav.clientHeight) {
+        nav.style.position = 'fixed';
+        nav.style.top = '0px';
+        document.body.style.paddingTop = nav.clientHeight + 'px';
+    } else {
+        nav.style.position = 'block';
+        document.body.style.paddingTop = '0';
+    }
+}
 // Event listeners
+window.addEventListener('scroll', handleNav);
