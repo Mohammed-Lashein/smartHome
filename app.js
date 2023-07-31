50cd2f0da40dce2bab5ef45c0d4ea61ebb2673b3
Mohammed Lashein <ben10gravattack@gmail.com>
2023-07-31 18:28:55 +0300

    finished the responsive design for phones

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const header = document.querySelector('header');
 console.log(header);
 
 const linksList = document.querySelector('ul.links');
+const linksListChildren = Array.from(linksList.children);
 const menuBars = document.querySelector('.menu-bars');
 const closeBtn = document.querySelector('.close');
 // Functions
@@ -30,8 +31,12 @@ function handleNavFixation() {
         
         nav.style.position = 'fixed';
         nav.style.top = '0px';
+        nav.style.borderBottom = '1px solid white';
+        nav.style.boxShadow = '1rem 1rem 10px rgba(0, 0, 0, 0.1)';
     } else {
         nav.style.position = 'block';
+        nav.style.borderBottom = 'none';
+        nav.style.boxShadow = '0 0 0 0';
     }
 }
 
@@ -41,10 +46,28 @@ function toggleNav() {
         menuBars.classList.toggle('active');
         closeBtn.classList.toggle('active');
         linksList.classList.toggle('hide');
-
     }
 }
 // Event listeners
 window.addEventListener('scroll', handleNavFixation);
 menuBars.addEventListener('click', toggleNav);
 closeBtn.addEventListener('click', toggleNav);
+linksListChildren.forEach((link) => {
+    link.addEventListener('click', () => {
+        linksList.classList.toggle('hide');
+        menuBars.classList.toggle('active');
+        closeBtn.classList.toggle('active');
+    })
+})
+// handle scrolling to specific sections using fixed nav
+linksListChildren.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        let id = e.target.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        const position = element.offsetTop;
+        window.scrollTo({
+            top: position - nav.getBoundingClientRect().height,
+        })
+    })
+})
